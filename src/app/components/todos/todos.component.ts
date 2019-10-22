46149ecf88efcf7913e106646c542e1a947216b7
r.kosiada <r.kosiada@hirschtec.eu>
2019-10-22 16:22:15 +0200

    Fill array of todos in ts file

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -7,12 +7,30 @@ import { Todo } from '../../models/Todo';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
+  // creating an array of todos as a TypeScript model 'Todo' 
   todos: Todo[];
 
   constructor() { }
 
   ngOnInit() {
+    this.todos = [
+      {
+        id: 1,
+        title: 'Todo One',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Todo Two',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Todo Three',
+        completed: false
+      },
+    
+    ]
   }
 
 }
