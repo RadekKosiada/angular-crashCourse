0140a351041dd68bd590ad21b3f6e30164ee3f29
r.kosiada <r.kosiada@hirschtec.eu>
2019-10-22 15:46:59 +0200

    Define a type (which we should do)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'this awesome app!';
+  //defining type
+  title: string = 'Crash course'
 }
