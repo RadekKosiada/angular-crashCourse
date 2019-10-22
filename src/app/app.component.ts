2d177db4ad47d430b74fea7f52ec9031ddc3362f
r.kosiada <r.kosiada@hirschtec.eu>
2019-10-22 15:53:37 +0200

    Clean the AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //defining type
-  title: string = 'Crash course';
-  // adding constructor method;
-  // runs before the content is rendered so it will display title from the constructor;
-  constructor() {
-    this.changeName("Some other title")
-  }
-  //creating a custom method
-  changeName(title: string): void {
-    this.title = title;
-  }
-
+  
 }
 
 
