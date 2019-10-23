0f41ce6f9573157f2e2adad7d5f0d354e8db6a25
r.kosiada <r.kosiada@hirschtec.eu>
2019-10-23 10:36:56 +0200

    Set PUT Request w/'completed' todos to server

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
 import { Todo } from 'src/app/models/Todo';
 
 @Component({
@@ -8,7 +9,8 @@ import { Todo } from 'src/app/models/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
-  constructor() { }
+
+  constructor(private todoService:TodoService) { }
 
   ngOnInit() {
   }
@@ -23,7 +25,10 @@ export class TodoItemComponent implements OnInit {
   }
 
   onToggle(todo) {
+    // toggle in UI
     todo.completed = !todo.completed;
+    // toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
   }
 
   onDelete(todo) {
