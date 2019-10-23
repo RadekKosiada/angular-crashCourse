fbd9a4d2ccd00cab35d69c1491a464e2105922d8
r.kosiada <r.kosiada@hirschtec.eu>
2019-10-23 14:47:04 +0200

    Add two new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
